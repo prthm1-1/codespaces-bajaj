82b71bb1a307f60064091ab8d5ab533925e001d4
Pratham Pandey <pratham1.pp@gmail.com>
2023-09-08 10:32:24 +0000

    pushc

--- index.js
+++ index.js
@@ -2,8 +2,48 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+app.use(express.urlencoded({
+  extended:true
+}))
+app.get('/bfhl', (req, res) => {
+ const response = {
+  "operation_code":1
+ }
+  res.status(200).send(response)
+})
+
+function isLetter(str) {
+  return str.length === 1 && str.match(/[a-z]/i);
+}
+
+app.post('/bfhl', (req, res) => {
+  const {data} = req.body
+  const response = {
+    "is_success":true,
+    "user_id":"pratham_pandey_2002",
+    "email":"pp1583@srmist.edu.in",
+    "roll_number":"RA2011026010177",
+    "alphabets":[],
+    "numbers":[],
+    "highest_alphabet":[]
+  }
+  let max = "A"
+  for(let i =0;i <data.length;i++){
+    if(isLetter(data[i])){
+      let c = data[i]
+      c = c.toUpperCase()
+      if(c.charCodeAt(0) > max.charCodeAt(0)){
+        max = c;
+      }
+      response.alphabets.push(c)
+      continue;
+    }
+    response.highest_alphabet.push(max)
+    response.numbers.push(data[i])
+  }
+
+  res.send(response)
 })
 
 app.listen(port, () => {
